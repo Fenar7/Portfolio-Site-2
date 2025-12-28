7267ac8d66a66d93baeaa3666c7f96d6a8ff885b
Fenar7 <fenarkhan@gmail.com>
2025-12-28 21:59:48 +0530

    header thing fix:

--- src/constants/index.js
+++ src/constants/index.js
@@ -211,11 +211,7 @@ export const projects = [{
     },
 ];
 export const socials = [
-    { name: "Instagram", href: "https://www.instagram.com/ali.sanatidev/reels/" },
-    {
-        name: "Youtube",
-        href: "https://www.youtube.com/channel/UCZhtUWTtk3bGJiMPN9T4HWA",
-    },
-    { name: "LinkedIn", href: "https://www.linkedin.com/in/ali-sanati/" },
-    { name: "GitHub", href: "https://github.com/Ali-Sanati" },
+    { name: "Instagram", href: "https://www.instagram.com/fenarkhan/" },
+    { name: "LinkedIn", href: "https://www.linkedin.com/in/fenarkhan/" },
+    { name: "GitHub", href: "https://github.com/Fenar7" },
 ];
